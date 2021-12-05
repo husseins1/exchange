b9d0e64f7cdab475ab6a9209398517a383e96e27
Hussein <hussendon3@gmail.com>
2021-12-05 21:34:07 +0300

    finished arabic version

--- script.js
+++ script.js
@@ -407,6 +407,8 @@ function getExchangeRate() {
 const lang = document.querySelector(".lang img");
 if(!document.body.classList.contains("ar")){
   lang.src = `https://flagcdn.com/48x36/iq.png`;
+}else{
+  lang.src = `https://flagcdn.com/48x36/us.png`;
 }
 
 // clocks
